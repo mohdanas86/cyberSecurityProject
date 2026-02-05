2771928591b5bc0e036d383c329d74f32f0bf19b
mohdanas86 <anasalam7739@gmail.com>
2026-02-05 16:17:32 +0530

    feat: initialize frontend project with Next.js, TypeScript, and Tailwind CSS
    - Added package.json for project dependencies and scripts
    - Created postcss.config.mjs for Tailwind CSS integration
    - Added tsconfig.json for TypeScript configuration

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <Button className="rounded-xs">Hello</Button>
+    </div>
+  );
+}
